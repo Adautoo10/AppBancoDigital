21aef91d8ed7f5717d3974870709daf92c8952d1
Usuario <Usuario@CPD02-M04>
2023-05-15 09:07:01 -0300

    Atualizando

--- AppBancoDigital/AppBancoDigital/View/AdicionarConta.xaml.cs
+++ AppBancoDigital/AppBancoDigital/View/AdicionarConta.xaml.cs
@@ -51,8 +51,17 @@ namespace AppBancoDigital.View
         }
 
 
-        private void btn_cadastrar_Clicked(object sender, EventArgs e)
+        private async void btn_cadastrar_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                await Navigation.PushAsync(new Conta());
+            }
+
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops, ocorreu um erro...", ex.Message, "OK");
+            }
 
         }
 
