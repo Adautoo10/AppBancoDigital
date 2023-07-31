4ac9c1ad36de3935b4ea9abddb5ee753917c6fca
Usuario <Usuario@CPD02-M04>
2023-07-31 08:49:23 -0300

    Criando Models e DataServices

--- AppBancoDigital/AppBancoDigital/Model/Transacao.cs
+++ AppBancoDigital/AppBancoDigital/Model/Transacao.cs
@@ -6,5 +6,10 @@ namespace AppBancoDigital.Model
 {
   public class Transacao
     {
+        public int Id { get; set; }
+        public Conta ContaOrigem { get; set; }
+        public Conta ContaDestino { get; set; }
+        public double Valor { get; set; }
+        public DateTime Data_Transacao { get; set; }
     }
 }
