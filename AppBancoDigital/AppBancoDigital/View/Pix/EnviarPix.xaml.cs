fbf2114355da73341b9d8c81e1b48ce52daec6f4
Usuario <Usuario@CPD02-M05>
2023-08-17 14:27:18 -0300

    Atualizando Design

--- AppBancoDigital/AppBancoDigital/View/Pix/EnviarPix.xaml.cs
+++ AppBancoDigital/AppBancoDigital/View/Pix/EnviarPix.xaml.cs
@@ -21,7 +21,7 @@ namespace AppBancoDigital.View.Pix
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            string teste = "Chave da Transferência: tiago@tiago.blog.br ";
+            string teste = "Chave da Transferência: Adauto.005 ";
 
             QRCodeGenerator qrGenerator = new QRCodeGenerator();
             QRCodeData qrCodeData = qrGenerator.CreateQrCode(teste, QRCodeGenerator.ECCLevel.Q);
